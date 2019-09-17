ef7b1ea11b853533bb1bc23f787bb653deb15875
malinkallen <github.ed5b79@m.insidarum.se>
2019-09-17 13:20:30 +0200

    Tests for creating notebook objects from invalid data.

--- Analyzer/test/NotebookTest.java
+++ Analyzer/test/NotebookTest.java
@@ -9,12 +9,12 @@ import org.junit.Test;
 public class NotebookTest {
 	
 	/**
-	 * Test that the correct number of code cells are found in JSON files.
+	 * Verify that the correct number of code cells are found in JSON files.
 	 */
 	@Test
 	public void testNumCodeCells() {
 		String dataDir = "testdata/count";
-		String[] files = {"empty.ipynb", "one.ipynb", "two.ipynb", "three_with_md.ipynb"};
+		String[] files = {"zero.ipynb", "one.ipynb", "two.ipynb", "three_with_md.ipynb"};
 		int[] numCodeCells = {0, 1, 2, 3};
 		for (int i=0; i<files.length; i++) {
 			String fileName = files[i];
@@ -22,12 +22,29 @@ public class NotebookTest {
 			try {
 				notebook = new Notebook(new File(dataDir + "/" + fileName));
 			} catch (ParseException e) {
-				System.out.println("ParseException when parsing " + fileName +": " + e.getMessage());
+				fail("ParseException when parsing " + fileName +": " + e.getMessage());
 			} catch (IOException e) {
-				System.out.println("IOException when parsing file: " + e.getMessage());
+				fail("IOException when parsing file: " + e.getMessage());
 			}
 			assertEquals("One code cell expected!", numCodeCells[i], notebook.numCodeCells());
 		}
 	}
 
+	/**
+	 * Verify that an IOException is thrown if we try to create a notebook from
+	 * a missing file.
+	 */
+	@Test (expected=IOException.class)
+	public void testParsingMissingFile() throws IOException, ParseException {
+		new Notebook(new File("nonexistent_file.txt"));
+	}
+	
+	/**
+	 * Verify that a ParseException is thrown if we try to create a notebook
+	 * from a file not containing JSON data.
+	 */
+	@Test (expected=ParseException.class)
+	public void testParsingEmptyFile() throws IOException, ParseException {
+		new Notebook(new File("testdata/count/empty.ipynb"));
+	}
 }
