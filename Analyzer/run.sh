9b1fa0d4d4eedad18501d9dd94b905f13d307d94
malinkallen <github.368c7b@m.insidarum.se>
2019-09-27 10:58:26 +0200

    Run script with better parameters and path.

--- Analyzer/run.sh
+++ Analyzer/run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -l
 
 #SBATCH -A snic2019-8-228
-#SBATCH -t 3:00:00
-#SBATCH -p core -n 16
+#SBATCH -t 140:00:00
+#SBATCH -p core -n 4
 #SBATCH -J notebook_analyzer
-#SBATCH -M snowy
 
-java -XX:+UseParallelGC -cp bin:./external/json-simple-1.1.1.jar Analyzer /home/maka4186/notebooks/df_over_100mb -count
-#java -XX:+UseParallelGC -cp bin:./external/json-simple-1.1.1.jar Analyzer /home/maka4186/notebooks -count # --> Fler timmar!!!
+java -XX:+UseParallelGC -cp bin:./external/json-simple-1.1.1.jar Analyzer /proj/uppstore2019098/notebooks -count
