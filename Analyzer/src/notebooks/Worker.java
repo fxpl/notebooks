47ea511c402c76c425a25da6c33594ebc00f27d3
malinkallen <github.ed5b79@m.insidarum.se>
2019-11-18 09:14:52 +0100

    Better reports for ClassCastExceptions.

--- Analyzer/src/notebooks/Worker.java
+++ Analyzer/src/notebooks/Worker.java
@@ -9,6 +9,11 @@ abstract class Worker<T> implements Callable<T> {
 		this.notebook = notebook;
 	}
 	
+	// TODO: Kan troligtvis tas bort så småningom!
+	String getNotebookName() {
+		return this.notebook.getName();
+	}
+	
 	/**
 	 * @return The value to return on failure.
 	 */
