1c59b5649897f27af64b575cabb0cc8b63f47ce2
malinkallen <github.ed5b79@m.insidarum.se>
2019-11-21 11:01:32 +0100

    Added program for dumping python snippets.

---
+++ Analyzer/src/notebooks/PythonDumper.java
@@ -0,0 +1,37 @@
+package notebooks;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PythonDumper {
+	
+	/**
+	 * Dump all snippets in the Python notebook stored in srcDir to a separate
+	 * file in targetDir. Every notebook in srcDir/somePath is stored in
+	 * targetDir/somePath. targetDir/somePath is created if needed.
+	 */
+	public void dump(String src, String target) throws NotebookException, IOException {
+		File srcFile = new File(src);
+		if (!srcFile.isDirectory()) {
+			// Create parent directory if needed
+			File targetDir = new File(target);
+			if (!targetDir.exists()) {
+				targetDir.mkdirs();
+			}
+			// Dump if Python file
+			Notebook srcNb = new Notebook(src);
+			if (Language.PYTHON.equals(srcNb.language())) {
+				srcNb.dumpCode(target, "py");
+			}
+		} else {
+			// This is a directory. Traverse.
+			String[] subFiles = srcFile.list();
+			String targetDirName = target + "/" + srcFile.getName();
+			for (String subFile: subFiles) {
+				dump(src + "/" + subFile, targetDirName);
+			}
+		}
+	}
+	
+
+}
