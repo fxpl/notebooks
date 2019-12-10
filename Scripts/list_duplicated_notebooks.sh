e6585c0105e120429fb28abe87d59c54bcea04c3
malinkallen <github.368c7b@m.insidarum.se>
2019-12-10 11:30:31 +0100

    Keep output in output directory.

--- Scripts/list_duplicated_notebooks.sh
+++ Scripts/list_duplicated_notebooks.sh
@@ -6,14 +6,17 @@
 #SBATCH -M snowy
 
 f2h=`./get_latest_output.sh "file2hashes"`
-duplicates=../duplicates_sorted.txt
+duplicates=../output/duplicates_sorted.txt
+mostDuplicated=../output/most_duplicated_notebooks.txt
 
 # For each combination of hashes (snippets), find the notebooks containing
 # this combination (in order). Sort the output on the number of notebooks
-# (descending) and write the result to most_duplicated_notebooks.txt
+# (descending) and write the result to the file specified above
+# ($mostDuplicated)
 sed -n "2,$ p" $f2h | cut -d' ' -f2- | egrep -v "\.ipynb" | sort | uniq -c | sort -rn > $duplicates
 
 sed -E 's/([0-9]) ([A-F,0-9])/\1, \2/' $duplicates | cut -d',' -f2- | while read hashComb
 do
-	egrep "ipynb\, $hashComb$" $f2h | cut -d',' -f1 | paste -sd' ' >> most_duplicated_notebooks.txt
+	egrep "ipynb\, $hashComb$" $f2h | cut -d',' -f1 | paste -sd' ' >> $mostDuplicated
 done
+echo "--------------------------------------------------------------------------------" >> $mostDuplicated
