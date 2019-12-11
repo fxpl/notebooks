b1ca932fe03c06ff21b8fc3af99a1f51b2e75a26
malinkallen <github.368c7b@m.insidarum.se>
2019-12-11 09:32:25 +0100

    Only print snippets that are at least 6 lines long.

--- Scripts/print_most_common_snippets.sh
+++ Scripts/print_most_common_snippets.sh
@@ -8,34 +8,44 @@
 hashFile=`./get_latest_output.sh "hash2files"`
 projPath=/proj/uppstore2019098/notebooks
 pathFile=$projPath/file_list.txt
-numSnippets=100
+numSnippets=100	# Number of snippets to print
+minLength=6	# Minimum number of lines in snippet
 
 i=1
 # Print the $numSnippets most common clones using SnippetPrinter (=> stdout at
 # the time of writing)
-sed -n "2,$ p" $hashFile | grep -o ',' -n | uniq -c | sort -rn | head -$numSnippets \
+sed -n "2,$ p" $hashFile | grep -o ',' -n | uniq -c | sort -rn \
  | cut -d':' -f1 | rev | cut -d' ' -f1 | rev | while read lineNum; do
 
  	lineNum=$(($lineNum+1))		# Since we skip header (read from line 2) above
 	line=`sed -n "$lineNum, $lineNum p" $hashFile`
+	length=`echo $line | cut -d',' -f2`
 
-	# Information about the snippet
-	notebook=`echo $line | cut -d',' -f2`
-	snippetIndex=`echo $line | cut -d',' -f3 | tr -d ' '`
-	notebookPath=`grep $notebook $pathFile | cut -d'/' -f2-`
-	notebookPath=$projPath"/"`grep $notebook $pathFile | cut -d'/' -f2-`
+	if [ $length -ge $minLength ];
+	then
+		# Information about the snippet
+		notebook=`echo $line | cut -d',' -f3`
+		snippetIndex=`echo $line | cut -d',' -f4 | tr -d ' '`
+		notebookPath=`grep $notebook $pathFile | cut -d'/' -f2-`
+		notebookPath=$projPath"/"`grep $notebook $pathFile | cut -d'/' -f2-`
 
-	# Number of occurrences of the snippet
-	numOccurrences=`grep "$notebook, $snippetIndex," $hashFile | grep -o ',' -n | uniq -c | rev | cut -d' ' -f2 | rev`
-	numOccurrences=`echo "$numOccurrences/2" | bc`
+		# Number of occurrences of the snippet
+		numOccurrences=`egrep "$notebook, $snippetIndex," $hashFile | grep -o ',' -n | uniq -c | rev | cut -d' ' -f2 | rev`
+		numOccurrences=`echo "($numOccurrences-1)/2" | bc`
 
-	# Print snippet
-	echo ""
-	echo "$i ($numOccurrences occurrences). $notebookPath($snippetIndex) :"
-	java -XX:+UseParallelGC -cp bin:./external/json-simple-1.1.1.jar \
-		notebooks.SnippetPrinter $notebookPath $snippetIndex
-	echo "--------------------------------------------------------------------------------"
+		# Print snippet
+		echo ""
+		echo "$i ($numOccurrences occurrences). $notebookPath($snippetIndex) :"
+		java -XX:+UseParallelGC -cp ../Programs/bin:../Programs/external/json-simple-1.1.1.jar \
+			notebooks.SnippetPrinter $notebookPath $snippetIndex
+		echo "--------------------------------------------------------------------------------"
 
-	i=$((i+1))
+		i=$((i+1))
+	fi
+
+	if [ $i -gt $numSnippets ];
+	then
+		exit
+	fi
  done
 
