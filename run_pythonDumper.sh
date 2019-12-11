e0941e13351c30fb27bb837291f6b227d909c38d
malinkallen <github.368c7b@m.insidarum.se>
2019-12-11 16:31:31 +0100

    Run PythonZipDumper instead of PythonDumper.

--- run_pythonDumper.sh
+++ run_pythonDumper.sh
@@ -3,9 +3,9 @@
 #SBATCH -A snic2019-8-228
 #SBATCH -t 70:00:00
 #SBATCH -p core -n 1
-#SBATCH -J notebook_pythonDumper
+#SBATCH -J notebook_pythonZipDumper
 
 java -XX:+UseParallelGC -cp Programs/bin:Programs/external/json-simple-1.1.1.jar \
-	notebooks.PythonDumper /proj/uppstore2019098/notebooks /proj/uppstore2019098/snippets
+	notebooks.PythonZipDumper /proj/uppstore2019098/notebooks /proj/uppstore2019098/snippets
 
 
