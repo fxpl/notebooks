c524d4589e6d6cc16d1376596b3d8d49522b583c
malinkallen <github.368c7b@m.insidarum.se>
2019-12-19 09:12:34 +0100

    More memory for PythonZipDumper.

--- run_pythonDumper.sh
+++ run_pythonDumper.sh
@@ -2,10 +2,10 @@
 
 #SBATCH -A snic2019-8-228
 #SBATCH -t 70:00:00
-#SBATCH -p core -n 1
+#SBATCH -p core -n 4
 #SBATCH -J notebook_pythonZipDumper
 
-java -XX:+UseParallelGC -cp Programs/bin:Programs/external/json-simple-1.1.1.jar \
+java -XX:+UseParallelGC -cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xmx24G \
 	notebooks.PythonZipDumper /proj/uppstore2019098/notebooks /proj/uppstore2019098/snippets
 
 
