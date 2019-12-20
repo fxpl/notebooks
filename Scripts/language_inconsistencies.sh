27875c1464e2b08c2380d98bf15bf96f3b038b84
malinkallen <github.368c7b@m.insidarum.se>
2019-12-20 08:50:45 +0100

    Script for finding inconsistent info about languages in notebooks.

---
+++ Scripts/language_inconsistencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2019-8-228
+#SBATCH -p core -n 1 
+#SBATCH -t 15:00
+#SBATCH -J lang_inconsistencies 
+#SBATCH --qos=short 
+
+langFile=`./get_latest_output.sh "all_languages"`
+sed -n "2,$ p" $langFile \
+	| awk -F ',' \
+	'{ if(\
+		(" UNKNOWN"!=$2 && " UNKNOWN"!=$3 && $2!=$3) \
+		|| (" UNKNOWN"!=$2 && " UNKNOWN"!=$4 && $2!=$4) \
+		|| (" UNKNOWN"!=$2 && " UNKNOWN"!=$6 && $2!=$6) \
+		|| (" UNKNOWN"!=$3 && " UNKNOWN"!=$4 && $3!=$4) \
+		|| (" UNKNOWN"!=$3 && " UNKNOWN"!=$6 && $3!=$6) \
+		|| (" UNKNOWN"!=$4 && " UNKNOWN"!=$6 && $4!=$6) \
+	) \
+	{print $1 " " $2 " " $3 " " $4 " " $6}}'
+
+
