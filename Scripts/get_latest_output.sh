1f269789c5863af51b62c7a22a07427ee9bc00da
malinkallen <github.368c7b@m.insidarum.se>
2020-01-07 09:43:50 +0100

    More consistent format of documentation.

--- Scripts/get_latest_output.sh
+++ Scripts/get_latest_output.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+################################################################################
 # Find the last file in ../Output whose name starts with the first argument to
 # this script. In this context 'last' means the last one when the files are
 # sorted by name in descending alphabetical order.
+################################################################################
 nameString=$1
 
 ls -1 ../Output/$nameString* | tail -1
