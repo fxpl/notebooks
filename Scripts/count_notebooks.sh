ffa01a8bb31bbd5ed05829194bc3697939811edf
malinkallen <github.368c7b@m.insidarum.se>
2020-01-07 09:07:59 +0100

    Clearer output from notebook counting.

--- Scripts/count_notebooks.sh
+++ Scripts/count_notebooks.sh
@@ -7,17 +7,15 @@
 #SBATCH -M snowy
 
 projDir="/proj/uppstore2019098"
-logFile="../logs/dumper-zip-python-complete.out"
+logFile="../Logs/analyze-all-complete.out"
 #TODO: Kör även efter flytt av oparsningsbart, och analyzern körd efter flytt
 
-processed=`find $projDir/notebooks -name "*ipynb" | wc -l`
+analyzed=`find $projDir/notebooks -name "*ipynb" | wc -l`
 forked=`find $projDir/forked_notebooks -name "*ipynb" | wc -l`
 unparseable=`find $projDir/unparseable_notebooks -name "*ipynb" | wc -l`
+unparseable_unmoved=`egrep "Skipping notebook\!" $logFile | wc -l`
 
-unparseable_unmoved=`egrep "Skipping\!" $logFile | wc -l`	# TODO: Ändra till "Skipping notebook\!" innan nästa körning!
-unparseable=`echo "$unparseable + ${unparseable_unmoved}" | bc`
-processed=`echo "$processed - ${unparseable_unmoved}" | bc`
-
-echo "Processed notebooks: $processed"	# TODO: Dubbelkolla med wc -l på output-fil
+echo "Analyzed notebooks: $analyzed"
+echo "Of these, processing failed for: ${unparseable_unmoved}"
 echo "Forked notebooks: $forked"
 echo "Unparseable notebooks: $unparseable"
