89fdf3e3a0e2248e40216f54a7e036143f9f9635
malinkallen <github.368c7b@m.insidarum.se>
2020-01-07 09:42:59 +0100

    Documentation, and counting of notebooks in output file.

--- Scripts/count_notebooks.sh
+++ Scripts/count_notebooks.sh
@@ -6,16 +6,25 @@
 #SBATCH -J count_notebooks
 #SBATCH -M snowy
 
+################################################################################
+# Count the number of notebooks in input directory, in output files and in
+# directories for forked and unparseable notebooks.
+################################################################################
+
 projDir="/proj/uppstore2019098"
 logFile="../Logs/analyze-all-complete.out"
+codeCells=`./get_latest_output.sh "code_cells"`
 #TODO: Kör även efter flytt av oparsningsbart, och analyzern körd efter flytt
 
-analyzed=`find $projDir/notebooks -name "*ipynb" | wc -l`
+outputLines=`wc -l $codeCells | cut -d' ' -f1`
+outputNotebooks=`echo "$outputLines - 1" | bc`
+inDir=`find $projDir/notebooks -name "*ipynb" | wc -l`
 forked=`find $projDir/forked_notebooks -name "*ipynb" | wc -l`
 unparseable=`find $projDir/unparseable_notebooks -name "*ipynb" | wc -l`
 unparseable_unmoved=`egrep "Skipping notebook\!" $logFile | wc -l`
 
-echo "Analyzed notebooks: $analyzed"
+echo "Notebooks in input directory: $inDir"
+echo "Analyzed notebooks: $outputNotebooks"
 echo "Of these, processing failed for: ${unparseable_unmoved}"
-echo "Forked notebooks: $forked"
-echo "Unparseable notebooks: $unparseable"
+echo "Forked (moved) notebooks: $forked"
+echo "Unparseable (moved) notebooks: $unparseable"
