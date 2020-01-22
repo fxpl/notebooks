c1af9b4665e596c4d449fdc32ad4506da91c09a4
malinkallen <github.368c7b@m.insidarum.se>
2020-01-22 15:58:21 +0100

    Don't allocate more cores than needed.

--- run_pythonDumper.sh
+++ run_pythonDumper.sh
@@ -2,10 +2,10 @@
 
 #SBATCH -A snic2019-8-228
 #SBATCH -t 70:00:00
-#SBATCH -p core -n 4
+#SBATCH -p core -n 2
 #SBATCH -J notebook_pythonZipDumper
 
-java -XX:+UseParallelGC -cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xmx24G \
+java -XX:+UseParallelGC -cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xmx12G \
 	notebooks.PythonZipDumper /proj/uppstore2019098/notebooks /proj/uppstore2019098/snippets
 
 
