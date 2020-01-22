fa1a046d21aa1658e164e39e04b4c54ae3b13ba7
malinkallen <github.368c7b@m.insidarum.se>
2020-01-22 16:00:46 +0100

    Count moved duplicates.

--- Scripts/count_notebooks.sh
+++ Scripts/count_notebooks.sh
@@ -14,17 +14,19 @@
 projDir="/proj/uppstore2019098"
 logFile="../Logs/analyze-all-complete.out"
 codeCells=`./get_latest_output.sh "code_cells"`
-#TODO: Kör även efter flytt av oparsningsbart, och analyzern körd efter flytt
 
 outputLines=`wc -l $codeCells | cut -d' ' -f1`
 outputNotebooks=`echo "$outputLines - 1" | bc`
 inDir=`find $projDir/notebooks -name "*ipynb" | wc -l`
+duplicated=`find $projDir/duplicated_notebooks -name "*ipynb" | wc -l`
 forked=`find $projDir/forked_notebooks -name "*ipynb" | wc -l`
 unparseable=`find $projDir/unparseable_notebooks -name "*ipynb" | wc -l`
 unparseable_unmoved=`egrep "Skipping notebook\!" $logFile | wc -l`
+# TODO: Kan samma notebook loggas som skippad flera gånger? Kör om med bara en analys för att få fram denna siffra!?
 
 echo "Notebooks in input directory: $inDir"
 echo "Analyzed notebooks: $outputNotebooks"
 echo "Of these, processing failed for: ${unparseable_unmoved}"
 echo "Forked (moved) notebooks: $forked"
 echo "Unparseable (moved) notebooks: $unparseable"
+echo "Duplicated (moved) notebooks: $duplicated"
