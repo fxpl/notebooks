644cc084ec6a3430fd90982354315606286d1d0b
malinkallen <github.ed5b79@m.insidarum.se>
2020-01-28 13:21:26 +0100

    Use 30 GB heap for NotebookAnalyzer.

--- run_notebookAnalyzer.sh
+++ run_notebookAnalyzer.sh
@@ -9,6 +9,6 @@ outputDirectory="Output"
 mkdir -p $outputDirectory
 
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
-	-cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xmx36G \
+	-cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xms30G -Xmx30G \
 	notebooks.NotebookAnalyzer --nb_path=/proj/uppstore2019098/notebooks --repro_file=/proj/uppstore2019098/notebooks/notebook-number_repo.csv --output_dir=$outputDirectory --all
 
