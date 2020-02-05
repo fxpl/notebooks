03cf5905e0fa927625821bb58cbc7c211c79cf23
malinkallen <github.368c7b@m.insidarum.se>
2020-02-05 11:10:29 +0100

    fxpl-stat:ified run script for NotebookAnalyzer.

--- run_notebookAnalyzer.sh
+++ run_notebookAnalyzer.sh
@@ -5,10 +5,13 @@
 #SBATCH -p core -n 6
 #SBATCH -J notebook_analyzer_all
 
+diskPath=/home/maka4186/notebook_disk
+nbPath=$diskPath/notebooks
+reproFile=$diskPath/notebook-number_repo.csv
 outputDirectory="Output"
 mkdir -p $outputDirectory
 
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
 	-cp Programs/bin:Programs/external/json-20190722.jar -Xms24G -Xmx24G \
-	notebooks.NotebookAnalyzer --nb_path=/proj/uppstore2019098/notebooks --repro_file=/proj/uppstore2019098/notebooks/notebook-number_repo.csv --output_dir=$outputDirectory --all
+	notebooks.NotebookAnalyzer --nb_path=$nbPath --repro_file=$reproFile --output_dir=$outputDirectory --all
 
