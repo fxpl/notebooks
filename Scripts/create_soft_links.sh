30de005c3d1e914b063ad0447598c647d4d86213
malinkallen <github.ed5b79@m.insidarum.se>
2020-02-21 13:01:26 +0100

    Actually create the links.

--- Scripts/create_soft_links.sh
+++ Scripts/create_soft_links.sh
@@ -9,5 +9,5 @@ files=( "code_cells" "loc" "languages" "cloneFrequency" "connections" )
 for file in ${files[@]};
 do
 	csvFile=`./get_latest_output.sh $file`
-	echo "ln -s $csvFile ../Output/$file.csv"
+	ln -s $csvFile ../Output/$file.csv
 done
