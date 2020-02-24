13e5a4ab1e55d3175a9b2b9095443fa1342677e2
malinkallen <github.ed5b79@m.insidarum.se>
2020-02-24 14:29:13 +0100

    Create links for file2Snippet files as well.

--- Scripts/create_soft_links.sh
+++ Scripts/create_soft_links.sh
@@ -8,8 +8,16 @@ prefixes=( "code_cells" "loc" "languages" "cloneFrequency" "connections" )
 
 for prefix in ${prefixes[@]};
 do
-	rm -f ../Output$prefix.csv
+	rm -f ../Output/$prefix.csv
 	csvFile=`./get_latest_output.sh $prefix`
 	ln -s $csvFile ../Output/$prefix.csv
 done
+
+filesPerSnippetNE=`./get_latest_output.sh filesPerSnippet2`
+rm -f ../Output/filesPerSnippetNE.csv
+ln -s $filesPerSnippetNE ../Output/filesPerSnippetNE.csv
+
+filesPerSnippetA=`ls -1 ../Output/filesPerSnippet2* | tail -2 | head -1`
+rm -f ../Output/filesPerSnippetA.csv
+ln -s $filesPerSnippetA ../Output/filesPerSnippetA.csv
 
