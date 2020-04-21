5f56836f5846dd8f176bf1febc86c2977c28e90a
malinkallen <github.368c7b@m.insidarum.se>
2020-04-21 10:21:46 +0200

    Run SccOutputAnalyzer on real data (with 28GB heap).

--- run_sccOutputAnalyzer.sh
+++ run_sccOutputAnalyzer.sh
@@ -5,9 +5,13 @@
 #SBATCH -p core -n 5
 #SBATCH -J scc_output_analyzer
 
+diskPath=/home/maka4186/notebook_disk
+reproFile=$diskPath/notebook-number_repo.csv
+pairFile=$diskPath/SourcererCC_output/clone.pairs
+statsFile=$diskPath/SourcererCC_output/files.stats
 outputDirectory="OutputSCC"
 mkdir -p $outputDirectory
 
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
-	-cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xmx36G \
-	notebooks.SccOutputAnalyzer --stats_file=Programs/test/data/scc/file_stats --repro_file=Programs/test/data/hash/repros.csv --pair_file=Programs/test/data/scc/clone_pairs --output_dir=$outputDirectory
+	-cp Programs/bin:Programs/external/json-simple-1.1.1.jar -Xms28G -Xmx28G \
+	notebooks.SccOutputAnalyzer --stats_file=$statsFile --repro_file=$reproFile --pair_file=$pairFile --output_dir=$outputDirectory
