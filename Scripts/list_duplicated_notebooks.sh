7e178ce3991177d07e9d099be39e1a9b77adf961
malinkallen <github.368c7b@m.insidarum.se>
2020-07-02 17:29:55 +0200

    Recreate clone list instead of appending to old one.

--- Scripts/list_duplicated_notebooks.sh
+++ Scripts/list_duplicated_notebooks.sh
@@ -36,6 +36,7 @@ listDuplicated() {
 
 	sed -E "s/([0-9]) ([A-F,0-9])/\1, \2/" $specs | cut -d',' -f1 > $count
 
+	rm $list
 	sed -E "s/([0-9]) ([A-F,0-9])/\1, \2/" $specs | grep -E -v "^\s*1\, " | cut -d',' -f2- | while read hashComb
 	do
 		grep -E "ipynb\, $hashComb$" $f2h | cut -d',' -f1 | paste -sd' ' >> $list
