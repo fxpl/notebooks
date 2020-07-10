77bea8ce5cebd2e49e923efd85c7c2f3b44ebee5
malinkallen <github.368c7b@m.insidarum.se>
2020-07-10 11:21:30 +0200

    run_postPreocessing.sh 1 step closer to working.

--- run_postProcessing.sh
+++ run_postProcessing.sh
@@ -5,36 +5,44 @@
 # NotebookAnalyzer and SccOutputAnalyzer.
 ################################################################################
 
+source paths.sh
+
 # Postprocessing for NotebookAnalyzer result
-ln -s OutputNBA Output
+ln -s $OutputNBA Output
 cd Scripts
 ./language_analysis.sh > ../Output/output_language_analysis.txt
 ./language_inconsistencies.sh > ../Output/output_language_inconsistencies.txt
-./count_nonempty_snippets.sh
-./clone_analysis.sh > ../Output/output_clone_analysis.txt
+./clone_analysis_nba.sh > ../Output/output_clone_analysis.txt
 ./print_most_common_snippets.sh 100 0 1 > ../Output/top100clones_min0.txt
 ./print_most_common_snippets.sh 100 4 1 > ../Output/top100clones_min4loc.txt
 ./list_duplicated_notebooks.sh
 
-# If a notebook is written in Python and exists in the file2hashes file in
-# OutputNBA but not in OutputSCC, it is empty. Find these and add to
-# file2hashes, cloneFrequency and connections files in OutputSCC (since empty
-# notebooks are not included in SourcererCC analysis).
-langFile=`./get_latest_output.sh "languages"`
+# Statistics for NotebookAnalyzer results
+./create_soft_links.sh	# TODO
+./get_notebook_sizes.sh
+Rscript statistics_paperIII_nba.R > ../Output/statistics_output.txt
+# Pack plots
+./reduce_large_images.sh
+cd ../Output
+tar -czf plots.tgz hist_clone_frequency*eps lang*eps log_hist_*eps cells*jpg *Inter_*jpg
+cd -
+
+# If a notebook is written in Python and is not included in the clone frequency
+# file in OutputSCC. it is empty. Find these and add to cloneFrequency and
+# connections files in OutputSCC (since empty notebooks are not included in
+# SourcererCC clones).
 pythonNotebooks="pythonNotebooks.txt"
+langFile=`./get_latest_output.sh "languages"`	# ../Output still points at NBA direcectory
 grep -E "^nb_[0-9]+\.ipynb\, PYTHON\, " $langFile | cut -d',' -f1 > $pythonNotebooks
 cd ..
 rm Output
-ln -s OutputSCC Output
+ln -s $OutputSCC Output
 cd Scripts
-file2hashesSCC=`./get_latest_output.sh "file2hashes"`
+sccNotebooks="sccNotebooks.txt"
 cloneFreqSCC=`./get_latest_output.sh "cloneFrequency"`
 connectionsSCC=`./get_latest_output.sh "connections"`
-cd ../Scripts
-sccNotebooks="sccNotebooks.txt"
-sed -n "2,$ p" $file2hashesSCC | cut -d',' -f1 > $sccNotebooks
+sed -n "2,$ p" $cloneFreqSCC | cut -d',' -f1 > $sccNotebooks
 grep -vFf sccNotebooks.txt pythonNotebooks.txt | while read notebook; do
-	echo "$notebook" >> $file2hashesSCC
 	echo "$notebook, 0, 0, 0, 0, 0, 0, 0" >> $cloneFreqSCC
 	echo "$notebook, 0, 0.0000, 0, 0.0000, 0, 0, 0.0000, 0.0000" >> $connectionsSCC
 done
@@ -42,12 +50,14 @@ rm $pythonNotebooks
 rm $sccNotebooks
 
 # Postprocessing for SourcererCC result
-# Note: This is not properly tested since we don't have the file2hashesNE yet
-./count_nonempty_snippets.sh
-./clone_analysis.sh > ../Output/output_clone_analysis.txt
-./print_most_common_snippets.sh 100 0 10 > ../Output/top100clones_min0.txt
-./print_most_common_snippets.sh 100 4 10 > ../Output/top100clones_min4loc.txt
-./list_duplicated_notebooks.sh
+./clone_analysis_scc.sh > ../Output/output_clone_analysis.txt
+
+#Statistics for SccOutputAnanlyzer results
+# TODO: Hur skapa mjuklänkar här?
+Rscript statistics_paperIII_scc.R > ../Output/statistics_output.txt
+./reduce_large_images.sh
+cd ../Output
+tar -czf plots.tgz hist_clone_frequency*eps log_hist_*eps cells*jpg *Inter_*jpg
 cd ..
 rm Output
 
