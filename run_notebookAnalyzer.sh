c2e6fab94296a037f70582572688a8d2e1c3585f
malinkallen <github.368c7b@m.insidarum.se>
2020-07-10 12:05:28 +0200

    Run full analysis with NotebookAnalyzer.

--- run_notebookAnalyzer.sh
+++ run_notebookAnalyzer.sh
@@ -6,5 +6,5 @@ outputDirectory=OutputNBA
 mkdir -p $outputDirectory
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
 	-cp Programs/bin:Programs/external/json-20190722.jar -Xms24G -Xmx24G \
-	notebooks.NotebookAnalyzer --nb_path=$nbPath --repro_file=$reproFile --output_dir=$outputDirectory --clones
+	notebooks.NotebookAnalyzer --nb_path=$nbPath --repro_file=$reproFile --output_dir=$outputDirectory --all
 
