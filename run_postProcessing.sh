f1d4e673a8cdfafab47d094131a364c927d58ebb
malinkallen <github.368c7b@m.insidarum.se>
2020-07-11 10:38:38 +0200

    Run postprocessing of of SccOutputAnalyzer results before NotebookAnalyzer results.

--- run_postProcessing.sh
+++ run_postProcessing.sh
@@ -7,32 +7,14 @@
 
 source paths.sh
 
-# Postprocessing for NotebookAnalyzer result
-ln -s $outputNBA Output
-cd Scripts
-./language_analysis.sh > ../Output/output_language_analysis.txt
-./language_inconsistencies.sh > ../Output/output_language_inconsistencies.txt
-./clone_analysis_nba.sh > ../Output/output_clone_analysis.txt
-./print_most_common_snippets.sh 100 0 1 > ../Output/top100clones_min0.txt
-./print_most_common_snippets.sh 100 4 1 > ../Output/top100clones_min4loc.txt
-./list_duplicated_notebooks.sh
-
-# Statistics for NotebookAnalyzer results
-./create_sym_links_nba.sh
-./get_notebook_sizes.sh
-Rscript statistics_paperIII_nba.R > ../Output/output_statistics.txt
-# Pack plots
-./reduce_large_images.sh
-cd ../Output
-tar -czf plots.tgz hist_clone_frequency*eps lang*eps log_hist_*eps cells*jpg *Inter_*jpg
-cd -
-
 # If a notebook is written in Python and is not included in the clone frequency
 # file in OutputSCC, it is empty. Find these and add to cloneFrequency and
 # connections files in OutputSCC (since empty notebooks are not included in
 # SourcererCC clones).
+ln -s $outputNBA Output
+cd Scripts
 pythonNotebooks="../pythonNotebooks.txt"
-langFile=`./get_last_output.sh "languages"`	# ../Output still points at NBA direcectory
+langFile=`./get_last_output.sh "languages"`
 grep -E "^nb_[0-9]+\.ipynb\, PYTHON\, " $langFile | cut -d',' -f1 > $pythonNotebooks
 cd ..
 rm Output
@@ -59,6 +41,28 @@ Rscript statistics_paperIII_scc.R > ../Output/statistics_output.txt
 ./reduce_large_images.sh
 cd ../Output
 tar -czf plots.tgz hist_clone_frequency*eps log_hist_*eps cells*jpg *Inter_*jpg
+cd ..
+rm Output
+
+
+# Postprocessing for NotebookAnalyzer result
+ln -s $outputNBA Output
+cd Scripts
+./language_analysis.sh > ../Output/output_language_analysis.txt
+./language_inconsistencies.sh > ../Output/output_language_inconsistencies.txt
+./clone_analysis_nba.sh > ../Output/output_clone_analysis.txt
+./print_most_common_snippets.sh 100 0 1 > ../Output/top100clones_min0.txt
+./print_most_common_snippets.sh 100 4 1 > ../Output/top100clones_min4loc.txt
+./list_duplicated_notebooks.sh
+
+# Statistics for NotebookAnalyzer results
+./create_sym_links_nba.sh
+./get_notebook_sizes.sh
+Rscript statistics_paperIII_nba.R > ../Output/output_statistics.txt
+# Pack plots
+./reduce_large_images.sh
+cd ../Output
+tar -czf plots.tgz hist_clone_frequency*eps lang*eps log_hist_*eps cells*jpg *Inter_*jpg
 cd ..
 rm Output
 
