cc65206b8c2568d1c73bbec247bb45d24cce8390
malinkallen <github.368c7b@m.insidarum.se>
2020-07-16 15:32:48 +0200

    Use 30 GB heap for SccOutputAnalyzer.

--- run_sccOutputAnalyzer.sh
+++ run_sccOutputAnalyzer.sh
@@ -7,5 +7,5 @@ outputDir=$outputSOA
 
 mkdir -p $outputDirectory
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
-	-cp Programs/bin -Xms28G -Xmx28G \
+	-cp Programs/bin -Xms30G -Xmx30G \
 	notebooks.SccOutputAnalyzer --stats_file=$statsFile --repro_file=$reproFile --pair_file=$pairFile --output_dir=$outputDir --tmp_dir=$tmpDir
