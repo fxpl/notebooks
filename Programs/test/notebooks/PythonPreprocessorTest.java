0dc8ddf339662094780619911c0d677431f49d02
malinkallen <github.ed5b79@m.insidarum.se>
2020-09-21 10:54:40 +0200

    Removed index from calls to add for expected result.

--- Programs/test/notebooks/PythonPreprocessorTest.java
+++ Programs/test/notebooks/PythonPreprocessorTest.java
@@ -15,7 +15,7 @@ public class PythonPreprocessorTest {
 	public void testProcess_oneSimpleLine() {
 		String[] str = {"abc\n"};
 		List<String> expectedOutput = new ArrayList<String>(1);
-		expectedOutput.add(0, str[0]);
+		expectedOutput.add(str[0]);
 		
 		processAndCheck(str, expectedOutput);
 	}
@@ -24,8 +24,8 @@ public class PythonPreprocessorTest {
 	public void testProcess_twoSimpleLines() {
 		String[] strings = {"abc\n", "def\n"};
 		List<String> expectedOutput = new ArrayList<String>(2);
-		expectedOutput.add(0, strings[0]);
-		expectedOutput.add(1, strings[1]);
+		expectedOutput.add(strings[0]);
+		expectedOutput.add(strings[1]);
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -33,11 +33,11 @@ public class PythonPreprocessorTest {
 	public void testProcess_severalStatements() {
 		String[] strings = {"import numpy as np; import pandas as pd\n", "b = np.sin(a)\n", "c = np.cos(b); d = np.tan(c)\n"};
 		List<String> expectedOutput = new ArrayList<String>(5);
-		expectedOutput.add(0, "import numpy as np");
-		expectedOutput.add(1, " import pandas as pd\n");
-		expectedOutput.add(2, "b = np.sin(a)\n");
-		expectedOutput.add(3, "c = np.cos(b)");
-		expectedOutput.add(4, " d = np.tan(c)\n");
+		expectedOutput.add("import numpy as np");
+		expectedOutput.add(" import pandas as pd\n");
+		expectedOutput.add("b = np.sin(a)\n");
+		expectedOutput.add("c = np.cos(b)");
+		expectedOutput.add(" d = np.tan(c)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -47,13 +47,13 @@ public class PythonPreprocessorTest {
 				"b = np.sin(a)\n", "'''Some other \n", "multi line string'''\n", "c = np.cos(b) '''And now, a string with # and \n and \\n and \\'\\'\\' to test\n'''\n",
 				"d = np.tan(c)'''Some string'''e = np.exp(d)\n"};
 		List<String> expectedOutput = new ArrayList<String>(7);
-		expectedOutput.add(0, "import numpy as np ");
-		expectedOutput.add(1, "import pandas as pd\n");
-		expectedOutput.add(2, "b = np.sin(a)\n");
-		expectedOutput.add(3, "\n");
-		expectedOutput.add(4, "c = np.cos(b) \n");
-		expectedOutput.add(5, "d = np.tan(c)");
-		expectedOutput.add(6, "e = np.exp(d)\n");
+		expectedOutput.add("import numpy as np ");
+		expectedOutput.add("import pandas as pd\n");
+		expectedOutput.add("b = np.sin(a)\n");
+		expectedOutput.add("\n");
+		expectedOutput.add("c = np.cos(b) \n");
+		expectedOutput.add("d = np.tan(c)");
+		expectedOutput.add("e = np.exp(d)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -62,10 +62,10 @@ public class PythonPreprocessorTest {
 		String[] strings = {"import numpy as np '''This is a string with \" and ' inside.''' import pandas as pd\n",
 		"b = np.sin(a) \"\"\"This is a string with \" and ' inside.\"\"\" c = np.cos(b)\n"};
 		List<String> expectedOutput = new ArrayList<String>(4);
-		expectedOutput.add(0, "import numpy as np ");
-		expectedOutput.add(1, " import pandas as pd\n");
-		expectedOutput.add(2, "b = np.sin(a) ");
-		expectedOutput.add(3, " c = np.cos(b)\n");
+		expectedOutput.add("import numpy as np ");
+		expectedOutput.add(" import pandas as pd\n");
+		expectedOutput.add("b = np.sin(a) ");
+		expectedOutput.add(" c = np.cos(b)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -74,10 +74,10 @@ public class PythonPreprocessorTest {
 		String[] strings = {"import numpy as np '''This is a string with \"\"\" inside.''' import pandas as pd\n",
 				"b = np.sin(a) \"\"\"This is a string with ''' inside.\"\"\" c = np.cos(b)\n"};
 		List<String> expectedOutput = new ArrayList<String>(4);
-		expectedOutput.add(0, "import numpy as np ");
-		expectedOutput.add(1, " import pandas as pd\n");
-		expectedOutput.add(2, "b = np.sin(a) ");
-		expectedOutput.add(3, " c = np.cos(b)\n");
+		expectedOutput.add("import numpy as np ");
+		expectedOutput.add(" import pandas as pd\n");
+		expectedOutput.add("b = np.sin(a) ");
+		expectedOutput.add(" c = np.cos(b)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -87,13 +87,13 @@ public class PythonPreprocessorTest {
 				"b = np.sin(a)\n", "'Some other string '\n", "c = np.cos(b) 'And now, a string with # and \n and \\n and \\' to test\n'\n",
 				"d = np.tan(c)'Some string'e = np.exp(d)\n"};
 		List<String> expectedOutput = new ArrayList<String>(7);
-		expectedOutput.add(0, "import numpy as np ");
-		expectedOutput.add(1, "import pandas as pd\n");
-		expectedOutput.add(2, "b = np.sin(a)\n");
-		expectedOutput.add(3, "\n");
-		expectedOutput.add(4, "c = np.cos(b) \n");
-		expectedOutput.add(5, "d = np.tan(c)");
-		expectedOutput.add(6, "e = np.exp(d)\n");
+		expectedOutput.add("import numpy as np ");
+		expectedOutput.add("import pandas as pd\n");
+		expectedOutput.add("b = np.sin(a)\n");
+		expectedOutput.add("\n");
+		expectedOutput.add("c = np.cos(b) \n");
+		expectedOutput.add("d = np.tan(c)");
+		expectedOutput.add("e = np.exp(d)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -121,10 +121,10 @@ public class PythonPreprocessorTest {
 				"c = np.cos(b)\n"
 		};
 		List<String> expectedOutput = new ArrayList<String>(4);
-		expectedOutput.add(0, "import numpy as np \n");
-		expectedOutput.add(1, "import matplotlib.pyplot as plt\n");
-		expectedOutput.add(2, "b = np.sin(a) \n");
-		expectedOutput.add(3, "c = np.cos(b)\n");
+		expectedOutput.add("import numpy as np \n");
+		expectedOutput.add("import matplotlib.pyplot as plt\n");
+		expectedOutput.add("b = np.sin(a) \n");
+		expectedOutput.add("c = np.cos(b)\n");
 		processAndCheck(strings, expectedOutput);
 	}
 	
@@ -150,14 +150,13 @@ public class PythonPreprocessorTest {
 	public void testProcess_excapedNewlineInComment() {
 		String[] strings = {"abc #\\\n", "def\n"};
 		List<String> expectedOutput = new ArrayList<String>(2);
-		expectedOutput.add(0, strings[0]);
-		expectedOutput.add(1, strings[1]);
+		expectedOutput.add(strings[0]);
+		expectedOutput.add(strings[1]);
 		processAndCheck(strings, expectedOutput);
 	}
 	
 	/*
 	 * TODO:
-	 * Index i add.
 	 * Mergea alla strängar (OK)
 	 * Ta bort (i ordning):
 	 * Trippelfnuttade strängar (OK)
