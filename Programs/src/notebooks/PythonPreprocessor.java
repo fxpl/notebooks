ece0461dcb279f4957e697b5790f2a3483330404
malinkallen <github.ed5b79@m.insidarum.se>
2020-09-22 09:49:45 +0200

    Extracted methods, commented, restructured, made methods static when applicable.

--- Programs/src/notebooks/PythonPreprocessor.java
+++ Programs/src/notebooks/PythonPreprocessor.java
@@ -6,12 +6,68 @@ import java.util.List;
 import org.json.JSONArray;
 
 public class PythonPreprocessor {
+	// TODO: Är det rimligt att ha dessa som instansvariabler?
 	JSONArray input;
 	List<String> processed;
 	
 	public PythonPreprocessor(JSONArray input) {
 		this.input = input;
 		processed = new ArrayList<String>();
+	}
+	
+	/**
+	 * Merge all strings in input
+	 * @return A single string containing all strings in input, in order
+	 */
+	public String mergeInputStrings() {
+		String code = "";
+		for (int i=0; i<input.length(); i++) {
+			code += input.getString(i);
+		}
+		return code;
+	}
+	
+	/**
+	 * Remove all escaped newlines from code
+	 * @param code A piece of code to remove escaped newlines from
+	 * @return A copy of the code with all escaped newlines removed
+	 */
+	public String removeEscapedNewLines(String code) {
+		String result = "";
+		int index = 0;
+		while (index < code.length()-1) {
+			if ('\\' == code.charAt(index) && '\n' == code.charAt(index+1)) {
+				index += 2;
+				result += ' ';
+			} else {
+				result += code.charAt(index++);
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Remove all newlines that occur between brackets
+	 * @param code A piece of code to remove newlines from
+	 * @return A copy of code with newlines between brackets removed
+	 */
+	public String removeNewlinesInBrackets(String code) {
+		String result = "";
+		int bracketLevel = 0;
+		int index = 0;
+		// Comments and strings are already removed
+		while (index < code.length()) {
+			if ('(' == code.charAt(index)) {
+				bracketLevel++;
+			} else if (')' == code.charAt(index)) {
+				bracketLevel--;
+			} else if ('\n' == code.charAt(index) && 0 < bracketLevel) {
+				index++;
+				continue;
+			}
+			result += code.charAt(index++);
+		}
+		return result;
 	}
 	
 	/**
@@ -77,7 +133,7 @@ public class PythonPreprocessor {
 	 * @param seq Char sequence to look for
 	 * @return true if the specified char sequence can be found at position pos in str
 	 */
-	private boolean charSequenceFound(String str, int pos, String seq) {
+	private static boolean charSequenceFound(String str, int pos, String seq) {
 		for (int i=0; i<seq.length(); i++) {
 			if (pos+i >= str.length() || str.charAt(pos+i) != seq.charAt(i)) {
 				return false;
@@ -90,7 +146,7 @@ public class PythonPreprocessor {
 	 * @param bools Array of booleans
 	 * @return true if any of the values in the argument is true
 	 */
-	private boolean anyTrue(boolean[] bools) {
+	private static boolean anyTrue(boolean[] bools) {
 		for (boolean inside: bools) {
 			if (inside) {
 				return true;
@@ -104,7 +160,7 @@ public class PythonPreprocessor {
 	 * @param current Value to skip
 	 * @return true if any of the values in bool, except bool[current], is true
 	 */
-	private boolean otherTrue(boolean[] bool, int current) {
+	private static boolean otherTrue(boolean[] bool, int current) {
 		for (int i=0; i<bool.length; i++) {
 			if (bool[i] && i != current) {
 				return true;
@@ -113,56 +169,12 @@ public class PythonPreprocessor {
 		return false;
 	}
 	
-	public String removeEscapedNewLines(String code) {
-		String result = "";
-		int index = 0;
-		while (index < code.length()-1) {
-			if ('\\' == code.charAt(index) && '\n' == code.charAt(index+1)) {
-				index += 2;
-				result += ' ';
-			} else {
-				result += code.charAt(index++);
-			}
-		}
-		return result;
-	}
-	
-	public String removeNewlinesInBrackets(String code) {
-		String result = "";
-		int bracketLevel = 0;
-		int index = 0;
-		// Comments and strings are already removed
-		while (index < code.length()) {
-			if ('(' == code.charAt(index)) {
-				bracketLevel++;
-			} else if (')' == code.charAt(index)) {
-				bracketLevel--;
-			} else if ('\n' == code.charAt(index) && 0 < bracketLevel) {
-				index++;
-				continue;
-			}
-			result += code.charAt(index++);
-		}
-		return result;
-	}
-
 	/**
-	 * Remove strings from input, and split all elements at newline and ';'.
-	 * @return A list of sublines
+	 * Split a piece of code at newlines and semicolons. Put each substring in
+	 * the list processed. Keep newlines, but not semicolons.
+	 * @param code A piece of code to split
 	 */
-	public List<String> process() {
-		// TODO: Bryt ut metod
-		String code = "";
-		for (int i=0; i<input.length(); i++) {
-			code += input.getString(i);
-		}
-		
-		code = removeStringsAndComments(code, new String[]{"\"\"\"", "'''"});
-		code = removeStringsAndComments(code, new String[]{"\"", "'"});
-		code = removeEscapedNewLines(code);
-		code = removeNewlinesInBrackets(code);
-
-		// TODO: Bryt ut metod
+	public void splitCode(String code) {
 		String[] lines = code.split("\n");
 		for (String line: lines) {
 			line += "\n";
@@ -171,6 +183,21 @@ public class PythonPreprocessor {
 				processed.add(statement);
 			}
 		}
+	}
+
+	/**
+	 * Remove strings from input, and split all elements at newline and ';'.
+	 * @return A list of sublines
+	 */
+	public List<String> process() {
+		String code = mergeInputStrings();
+		
+		code = removeStringsAndComments(code, new String[]{"\"\"\"", "'''"});
+		code = removeStringsAndComments(code, new String[]{"\"", "'"});
+		code = removeEscapedNewLines(code);
+		code = removeNewlinesInBrackets(code);
+
+		splitCode(code);
 		return processed;
 	}
 }
