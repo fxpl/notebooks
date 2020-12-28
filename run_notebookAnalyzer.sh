ea6b4b82d79b8096116738ba23cff35a494ad9bd
malinkallen <github.ed5b79@m.insidarum.se>
2020-12-28 10:19:50 +0100

    Updated run script for NotebookAnalyzer for paper IV runs.

--- run_notebookAnalyzer.sh
+++ run_notebookAnalyzer.sh
@@ -5,6 +5,6 @@ outputDir=$outputNBA
 
 mkdir -p $outputDir
 java -XX:+UnlockDiagnosticVMOptions -XX:NativeMemoryTracking=summary -XX:+PrintNMTStatistics -XX:+UseParallelGC \
-	-cp Programs/bin:Programs/python:Programs/external/json-20190722.jar -Xms24G -Xmx24G \
-	notebooks.NotebookAnalyzer --nb_path=$nbPath --repro_file=$reproFile --output_dir=$outputDir --modules
+	-cp Programs/bin:Programs/python:Programs/external/json-20190722.jar -Xms30G -Xmx30G \
+	notebooks.NotebookAnalyzer --nb_path=$nbPath --output_dir=$outputDir --modules
 
